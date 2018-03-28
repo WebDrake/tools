e3b55c947434c440f64f3fa62ce4d0a27a8a98bd
Joseph Rushton Wakeling <joe@webdrake.net>
2018-03-28 22:24:51 +0200

    rdmd: add testable function for calculating temp dir to use
    This new `rdmdTempDir` function has a similar purpose to the existing
    `myOwnTempDir`, but has two key differences:
    
      * it bases its output on a provided `RDMDArgs` instance rather
        than global state
    
      * it does not create the directory, only determines its path
    
    This enables us to create minimal unittests of the expected behaviour:
    
      * if the `RDMDArgs.userTempDir` field is set, and is a valid
        path, it will be used
    
      * otherwise, an auto-generated but still valid path will be
        provided

--- rdmd/jobs.d
+++ rdmd/jobs.d
@@ -3,3 +3,72 @@ Functionality responsible for determining the parameters
 of an rdmd build job
 */
 module rdmd.jobs;
+
+import rdmd.args;
+
+/**
+Calculate the path to the temporary directory for an
+rdmd build job
+
+Params:
+    args = parsed arguments settings
+
+Returns:
+    path to the temporary directory rdmd should use
+*/
+string rdmdTempDir(in RDMDArgs args)
+out (result)
+{
+    import std.path : isValidPath;
+    assert(result.isValidPath);
+}
+body
+{
+    import std.file : tempDir;
+    import std.format : format;
+    import std.path : buildPath;
+
+    if (args.userTempDir.length > 0)
+    {
+        import std.exception : enforce;
+        import std.path : isValidPath;
+        enforce(args.userTempDir.isValidPath,
+            "Specified tempdir is not a valid path: '%s'"
+                .format(args.userTempDir));
+
+        return args.userTempDir;
+    }
+
+    version (Posix)
+    {
+        import core.sys.posix.unistd : getuid;
+        return buildPath(tempDir(), ".rdmd-%d".format(getuid()));
+    }
+    else
+    {
+        import std.array : replace;
+        import std.path : dirSeparator;
+        return tempDir().replace("/", dirSeparator).buildPath(".rdmd");
+    }
+}
+
+unittest
+{
+    RDMDArgs args;
+
+    // result is non-pure and context-dependent so we just
+    // validate the basic logic: if args.userTempDir is set,
+    // we get it back, otherwise we get back a different
+    // but still valid directory
+    assert(args.rdmdTempDir().isValidPath);
+
+    args.userTempDir = "";
+    assert(args.rdmdTempDir().isValidPath);
+
+    args.userTempDir = " ";  // not a valid path
+    import std.exception : assertThrown;
+    assertThrown(args.rdmdTempDir());
+
+    args.userTempDir = "mytmp";
+    assert(args.rdmdTempDir() == "mytmp");
+}
